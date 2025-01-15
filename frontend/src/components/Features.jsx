bc8533f5f61ff2a3cbdb79901b0ff81259a5af0e
kush34 <chattkush34@gmail.com>
2025-01-15 14:14:50 +0530

    added hamburger menu for Mobile View

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -8,35 +8,55 @@ const Features = () => {
         </div>
         <ul className='my-10'>
             <li>
-                <div className='h-48 flex justify-around items-center'>
-                    <div className="text-2xl font-medium flex flex-col gap-4">
-                        Live Collabration
-                        <div className="text-sm w-72">The live collab feature allows real-time document editing and collaboration, enabling seamless teamwork and instant updates.</div>
+                <div className="h-auto md:h-48  flex flex-col md:flex-row justify-around items-center gap-4 p-4">
+                    <div className="text-2xl font-medium flex flex-col gap-4 text-center md:text-left">
+                    Live Collaboration
+                    <div className="text-sm w-72">
+                        The live collab feature allows real-time document editing and collaboration, enabling seamless teamwork and instant updates.
                     </div>
-                    <div className=''>
-                        <img src="/collab.png" width={200} height={200} alt="collab illustrations" />
+                    </div>
+                    <div className=" flex-shrink-0">
+                    <img
+                        src="/collab.png"
+                        width={200}
+                        height={200}
+                        alt="collab illustrations"
+                        className="object-cover"
+                    />
                     </div>
                 </div>
             </li>
             <li>
-                <div className='h-48 flex justify-around items-center'>
-                    <div className="text-2xl font-medium flex flex-col gap-4">
+                <div className='h-auto md:h-48  flex flex-col md:flex-row justify-around items-center gap-4 p-4'>
+                    <div className="text-2xl font-medium flex flex-col gap-4 text-center md:text-left">
                         Multiple Format 
                         <div className='text-sm w-72'>Edit all your documents in one place with  multiple file format support </div>
                     </div>
-                    <div className=''>
-                        <img src="/support.png" width={200} height={200} alt="collab illustrations" />
+                    <div className=' flex-shrink-0'>
+                        <img 
+                            src="/support.png" 
+                            width={150} 
+                            height={150} 
+                            alt="collab illustrations"
+                            className="object-cover"     
+                        />
+
                     </div>
                 </div>
             </li>
             <li>
-                <div className='h-48 flex justify-around items-center'>
-                    <div className="text-2xl font-medium flex flex-col gap-4">
+                <div className='h-auto md:h-48  flex flex-col md:flex-row justify-around items-center gap-4 p-4'>
+                    <div className="text-2xl font-medium flex flex-col gap-4 text-center md:text-left">
                         Offline Editing
                         <div className='text-sm w-72'>The offline feature enables document editing without an internet connection, syncing changes once back online.</div>
                     </div>
-                    <div className=''>
-                        <img src="/offline.png" width={200} height={200} alt="collab illustrations" />
+                    <div className=' flex-shrink-0'>
+                        <img src="/offline.png" 
+                            width={150} 
+                            height={150} 
+                            alt="collab illustrations" 
+                            className="object-cover"
+                        />
                     </div>
                 </div>
             </li>
