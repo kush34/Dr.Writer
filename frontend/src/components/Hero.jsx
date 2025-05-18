29539a8269832b683646c685dcee4878cfd2dc61
kush34 <chattkush34@gmail.com>
2025-05-19 01:22:37 +0530

    added background in landing page and made UI changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className=' 
+    <div className='z-10
     w-full h-[80vh] flex flex-col gap-10 justify-center items-center'>
-        <div className="title text-5xl text-center font-medium">
-            The Only Document <br></br>
+        <div className="title text-2xl md:text-5xl text-center font-medium">
+            The Only 
+            <span className='text-green-600 font-bold italic'> Document</span>  <br></br>
             Editor you need
         </div>
-        <button className='border rounded py-2 px-4 text-white-800 font-medium hover:bg-zinc-100 hover:text-black duration-250 ease-out'>
+        <a href='/login' className='border rounded py-2 px-4 text-white-800 font-medium hover:bg-zinc-100 hover:text-black  duration-250 ease-out'>
             Get Started
-        </button>
+        </a>
     </div>
   )
 }
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,20 +4,26 @@ import Footer from '@/components/Footer.jsx';
 import Features from '@/components/Features.jsx';
 import Plans from '@/components/Plans.jsx';
 import "./landing.css"
+import { BackgroundLines } from '@/components/BackgroundLines.jsx';
 const Landing = ()=>{
     
     return(
-        <div className='w-full text-white'>
-            <Navbar/>
-            <div className='flex justify-center items-center my-5'>
+        <div className='w-full text-white relative overflow-hidden'>
+            <div className="z-20 fixed flex w-full">
+                <Navbar/>
+            </div>
+            {/* <div className="z-0"> */}
+                <BackgroundLines/>
+            {/* </div> */}
+            <div className='z-20 h-screen flex justify-center items-center my-5'>
                 <Hero/>
             </div>
             <div className='mt-10'>
                 <Features/>
             </div>
-            <div className='mt-10'>
+            {/* <div className='mt-10'>
                 <Plans/>
-            </div>
+            </div> */}
             <div className='bottom-0 relative w-full'>
                 <Footer/>
             </div>
