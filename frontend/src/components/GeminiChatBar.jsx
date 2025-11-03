a2f1bcfb389b7adddaa1be2aa572857e3c936c0e
kush34 <chattkush34@gmail.com>
2025-11-03 18:53:05 +0530

    saving gemini chats on DB

--- frontend/src/components/GeminiChatBar.jsx
+++ frontend/src/components/GeminiChatBar.jsx
@@ -9,7 +9,7 @@ import {
     SidebarMenuItem,
     SidebarFooter,
 } from "@/components/ui/sidebar";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { SendHorizontal } from 'lucide-react';
 import { Input } from "@/components/ui/input";
 import React from "react";
@@ -22,97 +22,113 @@ import { ThemeContext } from "@/context/ThemeContext";
 import Markdown from "react-markdown";
 import { toast } from "@/hooks/use-toast";
 
-export function GeminiChatBar() {
+export function GeminiChatBar({ documentId }) {
     const { user, loading } = useContext(UserContext);
-    const {theme} = useContext(ThemeContext);
+    const { theme } = useContext(ThemeContext);
     if (loading) return <p>Loading...</p>;
     if (!user) return <p>User not logged in</p>;
-    const [prompt,setPrompt] = useState('');
-    const [geminiLoading,setGeminiLoading] = useState(false);
-    const [responseList,setResponseList] = useState([]);
+    const [prompt, setPrompt] = useState('');
+    const [geminiLoading, setGeminiLoading] = useState(false);
+    const [responseList, setResponseList] = useState([]);
 
+    const fetchChats = async () => {
+        try {
+            const response = await apiClient.get(`document/getChats/${documentId}`)
+            console.log(response.data)
+            setResponseList(response.data)
+        } catch (error) {
+            toast('Could not get Chats')
+            console.log('Fetching chats from GET document/getChats/:documentId ', error)
+        }
+    }
+    const handleSend = async () => {
 
-    const handleSend = async ()=>{
-        
-        if(prompt == '') return;
+        if (prompt == '') return;
         setGeminiLoading(true);
         setPrompt('');
         try {
-            const response = await apiClient.post('/document/userprompt',{
-                userPrompt:prompt
+            const response = await apiClient.post('/document/userprompt', {
+                userPrompt: prompt,
+                documentId
             });
             // console.log(response);
             if (response.status === 200) {
-                setResponseList([...responseList, prompt, response.data.rawText]);
-            }else{
+                setResponseList([...responseList, response.data]);
+            } else {
                 toast("Could not send your prompt");
             }
-            
+
         } catch (error) {
             toast("Something went wrong");
-            
-        }finally{
+
+        } finally {
             setGeminiLoading(false);
         }
     }
+    useEffect(() => {
+        fetchChats();
+    }, [])
     return (
-        <Sidebar className={`${theme=='dark' ? "bg-zinc-900":""}`}>
-        <SidebarContent className={`${theme=='dark' ? "bg-zinc-900":""}`}>
-            <SidebarGroup>
-                <SidebarGroupLabel className='text-xl font-semibold'>Dr. Writer</SidebarGroupLabel>
-            </SidebarGroup>
-            <SidebarContent>
-                <div className="overflow-y-scroll no-scrollbar">
-                    {responseList.length <= 0 ? (
-                        <div className="px-4 text-zinc-500">
-                        Enter prompt for Gemini response
-                    </div>
-                    ) : (
-                        responseList.map((item, index) => (
-                            <div key={index} className="space-y-2 m-2">
+        <Sidebar className={`${theme == 'dark' ? "bg-zinc-900" : ""}`}>
+            <SidebarContent className={`${theme == 'dark' ? "bg-zinc-900" : ""}`}>
+                <SidebarGroup>
+                    <SidebarGroupLabel className='text-xl font-semibold'>Dr. Writer</SidebarGroupLabel>
+                </SidebarGroup>
+                <SidebarContent>
+                    <div className="overflow-y-scroll no-scrollbar">
+                        {responseList.length <= 0 ? (
+                            <div className="px-4 text-zinc-500">
+                                Enter prompt for Gemini response
+                            </div>
+                        ) : (
+                            responseList.map((item, index) => (
+                                <div key={index} className="space-y-2 m-2">
 
-                        {/* Gemini message */}
-                        <div className="p-2 rounded-md text-sm bg-zinc-200 text-black">
-                            <Markdown>{item}</Markdown>
-                        </div>
-                        </div>
-                    ))
-                    )}
-                </div>
-            </SidebarContent>
-
-        </SidebarContent>
-        <SidebarFooter className={`${theme=='dark' ? "bg-zinc-900":""}`}>
-            <SidebarContent>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                    {geminiLoading ?
-                    <div>
-                        Loading...
+                                    {/* Gemini message */}
+                                    <div className="flex flex-col gap-2 p-2 rounded-md text-sm bg-zinc-400 text-black">
+                                        <Markdown>{item.prompt}</Markdown>
+                                    </div>
+                                    <div className="flex flex-col gap-2 p-2 rounded-md text-sm bg-zinc-200 text-black">
+                                        <Markdown>{item.response}</Markdown>
+                                    </div>
+                                </div>
+                            ))
+                        )}
                     </div>
-                    :
-                    <div className={`${theme=='dark' ? "bg-zinc-900":""} flex`}>
-                            <Input 
-                            id="prompt"
-                            type="text"
-                            className={`${theme=='dark' ? "bg-zinc-900":""} outline-none`}
-                            onChange={(e)=>setPrompt(e.target.value)} 
-                            value={prompt}
-                            placeholder='enter your prompt Gemini' 
-                            onKeyDown={(e)=>{
-                                if(e.key === 'Enter'){
-                                    handleSend();
-                                }
-                            }}
-                            />
-                            <SendHorizontal onClick={handleSend} className="text-xl cursor-pointer m-2"/>
-                        </div>
-                    
-                    }
-                    </SidebarMenuItem>
-                </SidebarMenu>
+                </SidebarContent>
+
             </SidebarContent>
-        </SidebarFooter>
+            <SidebarFooter className={`${theme == 'dark' ? "bg-zinc-900" : ""}`}>
+                <SidebarContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            {geminiLoading ?
+                                <div>
+                                    Loading...
+                                </div>
+                                :
+                                <div className={`${theme == 'dark' ? "bg-zinc-900" : ""} flex`}>
+                                    <Input
+                                        id="prompt"
+                                        type="text"
+                                        className={`${theme == 'dark' ? "bg-zinc-900" : ""} outline-none`}
+                                        onChange={(e) => setPrompt(e.target.value)}
+                                        value={prompt}
+                                        placeholder='enter your prompt Gemini'
+                                        onKeyDown={(e) => {
+                                            if (e.key === 'Enter') {
+                                                handleSend();
+                                            }
+                                        }}
+                                    />
+                                    <SendHorizontal onClick={handleSend} className="text-xl cursor-pointer m-2" />
+                                </div>
+
+                            }
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarContent>
+            </SidebarFooter>
         </Sidebar>
-);
+    );
 }
--- frontend/src/pages/EditorPageLayout.jsx
+++ frontend/src/pages/EditorPageLayout.jsx
@@ -4,13 +4,14 @@ import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
 import { GeminiChatBar } from "../components/GeminiChatBar";
 import './printer.css'
 import { ThemeContext } from "@/context/ThemeContext";
+import { useParams } from "react-router-dom";
 const Layout = ({ children }) => {
   const { theme } = useContext(ThemeContext);
-
+  const {id} = useParams();
   return (
     <div className={`flex ${theme=='dark'?"bg-zinc-900":"bg-zinc-100"}`}>
       <SidebarProvider>
-        <GeminiChatBar className='geminichatbar'/>
+        <GeminiChatBar documentId={id} className='geminichatbar'/>
             <main className="flex-1 p-4">
               <SidebarTrigger className='trigger' />
               {children}
