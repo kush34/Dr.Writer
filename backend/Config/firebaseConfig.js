1930ba849ed0d760c3e9ffd3e4bd84ccbdb51235
kush34 <chattkush34@gmail.com>
2025-11-09 23:32:55 +0530

    simplifed firebaseConfig file: placed them at the same position on File structure

--- backend/Config/firebaseConfig.js
+++ backend/Config/firebaseConfig.js
@@ -1,10 +1,9 @@
 import admin from 'firebase-admin';
-// import serviceAccount from './secret-file.json' with { type: "json" }; //for Development
 import serviceAccount from '../secret-file.json' with { type: "json" }; //for production
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.databaseURL // Replace with your project's database URL
+  databaseURL: process.env.databaseURL 
 });
 
 export default admin;
